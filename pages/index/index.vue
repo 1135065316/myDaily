82fb1ff715134303b82bf81dd54490d145821b47
blkZ <1135065316@qq.com>
2020-08-01 21:49:07 +0800

    调整了首页的内容,并增加了service

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,19 +1,46 @@
 <template>
-	<view>
+	<view class="dflex-column container">
 		<image class="logo" src="/static/logo.png"/>
-		<button @tap="toDaily">日常记录</button>
-		<button @tap="toSport">自重健身</button>
+		<!-- <button @tap="toDaily">日常记录</button>
+		<button @tap="toSport">自重健身</button> -->
+		<!-- 记录提示 -->
+		<text>{{recordTip}}</text>
+		<!-- 记录盒子 -->
+		<view class="recordBox" v-if="!isRecord">
+			<text>存活?</text>
+			<radio-group @change='handleRecordChange'>
+				<radio :value="true" checked>是</radio>
+				<radio :value="false">否</radio>
+			</radio-group>
+			<button @tap="handleSubmit">提交</button>
+		</view>
+		<!-- 查看记录历史 -->
+		<button>查看历史</button>
 	</view>
 </template>
 
 <script>
+	import service from '@/service.js'
 	export default {
 		data() {
 			return {
-				
+				recordTip: '今日未记录',
+				isRecord: false, // 今日是否已经记录
+				myChoose: null, // 我的选择 true|false
 			}
 		},
 		methods: {
+			// 处理提交
+			handleSubmit() {
+				service.test((res)=>{
+					console.log(res);
+				})
+			},
+			// 处理记录改变
+			handleRecordChange(event) {
+				// 保存我的选择
+				this.myChoose = event.detail.value
+			},
 			toDaily() {
 				uni.navigateTo({
 				    url: '../myDaily/myDaily'
@@ -28,9 +55,12 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.logo {
 		height: 1rpx;
 		width: 1rpx;
+	}
+	.container {
+		width: 100vw;
 	}
 </style>
---
+++ service.js
@@ -0,0 +1,95 @@
+// 管理账号信息
+const USERS_KEY = 'USERS_KEY';
+const STATE_KEY = 'STATE_KEY';
+const web_url = "https://www.shitouji.ltd/index.php?datatype=json&";
+
+
+// 发出订单
+const test = function(callback) {
+	// let url = web_url + "ctrl=sjapp&action=svorder&@random@&datatype=json";
+	let url = "http://www.sword-z.xyz/mydaily?datatype=json&";
+	requestGet(url,'',(res)=>{
+		callback(res);
+	})
+}
+
+
+const requestGet = function(url, data, callback) {
+	uni.request({
+		url: url,
+		data: data,
+		header: {
+			'custom-header': 'application/json'
+		},
+		success: (res) => {
+			callback(res)
+		},
+		// 没用，要去manifest.json里面配置，因为manifest.json里的timeout不设置的话，默认值是30秒
+		// timeout: 100,
+		
+		fail: (res) => {
+			if (res.errMsg == "request:fail timeout") {
+				uni.showToast({
+					icon: 'none',
+					title: "408网络超时，请检查网络连接后重试，如仍无响应请联系上级",
+					duration: 1500
+				})
+			} else if (res.errMsg == "request:fail abort statusCode:-1") {
+				uni.showToast({
+					icon: 'none',
+					title: "404网络连接失败，请检查网络连接后重试，如仍无响应请联系上级",
+					duration: 1500
+				})
+			} else {
+				uni.showToast({
+					icon: 'none',
+					title: "未知网络错误：" + res.errMsg,
+					duration: 1500
+				})
+			}
+			callback(res)
+
+		},
+	})
+}
+const requestPost = function(url, data, callback) {
+	uni.request({
+		url: url,
+		data: data,
+		method: "POST",
+		header: {
+			'content-type': 'application/x-www-form-urlencoded'
+		},
+		success: (res) => {
+			callback(res)
+		},
+		fail: (res) => {
+			if (res.errMsg == "request:fail timeout") {
+				uni.showToast({
+					icon: 'none',
+					title: "408网络超时，请检查网络连接后重试，如仍无响应请联系上级",
+					duration: 1500
+				})
+			} else if (res.errMsg == "request:fail abort statusCode:-1") {
+				uni.showToast({
+					icon: 'none',
+					title: "404网络连接失败，请检查网络连接后重试，如仍无响应请联系上级",
+					duration: 1500
+				})
+			} else {
+				uni.showToast({
+					icon: 'none',
+					title: "未知网络错误：" + res.errMsg,
+					duration: 1500
+				})
+			}
+			callback(res)
+		
+		}
+	})
+}
+
+
+export default {
+	test
+}
